8d8fcf822cb40386f1c6ef920161727ee905f426
AlaaaEzzat <alaaazzetdd@outlook.sa>
2024-05-24 01:35:20 +0300

    HitEffect

--- Assets/Scripts/ShootArrow.cs
+++ Assets/Scripts/ShootArrow.cs
@@ -5,8 +5,27 @@ using UnityEngine;
 public class ShootArrow : MonoBehaviour
 {
     [SerializeField] private float arrowSpeed;
+    [SerializeField] GameObject hitEffect;
+
+    private void Start()
+    {
+        StartCoroutine(distroyArrowAfterTime());
+    }
     void Update()
     {
         transform.position += transform.forward * arrowSpeed * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Instantiate(hitEffect, collision.GetContact(0).point, Quaternion.identity);
+        Destroy(this.gameObject);
+    }
+
+    IEnumerator distroyArrowAfterTime()
+    {
+        yield return new WaitForSeconds(10);
+        Destroy(this.gameObject);
+
     }
 }
