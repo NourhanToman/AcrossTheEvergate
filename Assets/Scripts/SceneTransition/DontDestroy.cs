2651de1ba7dd37f2df6e76fde9a834a8640ebcd7
RodainaElrashidy <rodainaelrashidy000@gmail.com>
2024-05-31 08:26:58 +0300

    Fixed
    Systems game object was destroyed when a new scene starts so LoadManager and FadeCanvas were also destroyed. Fixed by adding a dontdestroy script to Systems.

---
+++ Assets/Scripts/SceneTransition/DontDestroy.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AccrossTheEvergate
+{
+    public class DontDestroy : MonoBehaviour
+    {
+        private void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
