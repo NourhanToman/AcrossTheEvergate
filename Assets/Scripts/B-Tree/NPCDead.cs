9b2a802e468272cbcca6855f032183ef54cbbef6
Nourhan Taman <147274340+NourhanToman@users.noreply.github.com>
2024-06-13 01:31:12 +0300

    update

--- Assets/Scripts/B-Tree/NPCDead.cs
+++ Assets/Scripts/B-Tree/NPCDead.cs
@@ -1,4 +1,5 @@
 using BehaviorDesigner.Runtime;
+using Fungus;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -10,13 +11,13 @@ namespace AccrossTheEvergate
 
     public class NPCDead : MonoBehaviour
     {
-       // [SerializeField] private Material _dissolveShader;
+        [SerializeField] private Flowchart _Chart;
+        [SerializeField] private Flowchart _ARChart;
         private BehaviorTree _NPCtree;
         private NavMeshAgent _Agent;
         [SerializeField] private GameObject Arachilion;
         private DissolvingController dissolve;
         private Animator animator;
-        private bool _attacked = false;
         private float _currentValue = 0f;
 
         private void Start()
@@ -27,44 +28,27 @@ namespace AccrossTheEvergate
             animator = GetComponent<Animator>();
         }
 
-        //counter for the number of attacks needed
-
-        void Update()
-        {
-            /*if (_attacked)
-            {
-                _currentValue = Mathf.Lerp(_currentValue, 1f, 1f * Time.deltaTime);
-                _dissolveShader.SetFloat("_Dissolve", _currentValue);
-                if (_currentValue >= 0.9f)
-                {
-                    _attacked = false;
-
-                }
-
-            }*/
-        }
-
-     /*   private void OnTriggerEnter(Collider other)
-        {
-            if (other.tag == "Arrow")
-            {
-               // _attacked = true;
-                _NPCtree.enabled = false;
-                Destroy(Arachilion.gameObject);
-            }
-        }
-*/
-        private void OnCollisionEnter(Collision collision)
+        private void OnCollisionEnter(UnityEngine.Collision collision)
         {
             if (collision.gameObject.CompareTag("Arrow"))
             {
-                Debug.Log("Attack");
-                // _attacked = true;
-                _Agent.speed = 0f;
-                _NPCtree.enabled = false;
-                animator.SetFloat("RUN", 0.0f, 0.0f, Time.deltaTime); 
-                dissolve.Dissolve();
-                Destroy(Arachilion.gameObject,2.0f);
+                if (_ARChart.GetBooleanVariable("isCollect"))
+                {
+                    _Chart.ExecuteBlock("isHit");
+                    if (_currentValue <= 0)
+                    {
+                        _currentValue++;
+                    }
+                }
+                else
+                {
+                    _Agent.speed = 0f;
+                    _NPCtree.enabled = false;
+                    animator.SetFloat("RUN", 0.0f, 0.0f, Time.deltaTime);
+                    dissolve.Dissolve();
+                    Destroy(Arachilion.gameObject, 2.0f);
+                    _ARChart.ExecuteBlock("Collect");
+                }
             }
         }
     }
